35187c4949137505e8ca76495ec231240d12521a
Igomesxr <silvasgustavogomes@gmail.com>
2023-08-31 16:53:38 -0400

    adicionei a rota produto
    save

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -9,6 +10,11 @@ app.get('/', (req, res) => {
   res.render('index', { message: 'Olá, Mundo!' });
 });
 
+app.get('/produto', (req, res) => {
+  res.render('produto', { message: 'Olá, kjyuoyiu!' });
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
