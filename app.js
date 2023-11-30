54f7c6e50acb5c8cfa9390c6df05ed88d9ab54d7
Igomesxr <silvasgustavogomes@gmail.com>
2023-11-30 16:40:16 -0400

    commit

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 const port = 3000;
 
 const produtos = [
-  {id: 1, nome: "Camisa Nike Preta", preco: "R$ 320", descricao: "Camisa bem boa", imagens: "/imagens/camisa_preta.webp"},
-  {id: 2, nome: "Camisa Nike Vinho", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_vinho.webp"},
-  {id: 3, nome: "Camisa Nike Azul", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_azul.webp"},
-  {id: 4, nome: "Camisa Nike Branca", preco: "R$ 450", descricao: "Camisa bem boa", imagens: "/imagens/camisa_branca.webp"},
-  {id: 5, nome: "Camisa Nike Branca", preco: "R$ 450", descricao: "Camisa bem boa", imagens: "/imagens/camisa_branca.webp"},
-  {id: 6, nome: "Camisa Nike Azul", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_azul.webp"},
-  {id: 7, nome: "Camisa Nike Vinho", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_vinho.webp"},
-  {id: 8, nome: "Camisa Nike Preta", preco: "R$ 320", descricao: "Camisa bem boa", imagens: "/imagens/camisa_preta.webp"},
-  {id: 9, nome: "Camisa Nike Vinho", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_vinho.webp"},
-  {id: 10, nome: "Camisa Nike Azul", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_azul.webp"},
+  {id: 1, nome: "Camisa Nike Preta", preco: "R$ 320", descricao: "Camisa bem boa", imagens: "/imagens/camisa_preta.png"},
+  {id: 2, nome: "Camisa Nike Vinho", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_vinho.png"},
+  {id: 3, nome: "Camisa Nike Azul", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_azul.png"},
+  {id: 4, nome: "Camisa Nike Branca", preco: "R$ 450", descricao: "Camisa bem boa", imagens: "/imagens/camisa_branca.png"},
+  {id: 5, nome: "Camisa Nike Branca", preco: "R$ 450", descricao: "Camisa bem boa", imagens: "/imagens/camisa_branca.png"},
+  {id: 6, nome: "Camisa Nike Azul", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_azul.png"},
+  {id: 7, nome: "Camisa Nike Vinho", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_vinho.png"},
+  {id: 8, nome: "Camisa Nike Preta", preco: "R$ 320", descricao: "Camisa bem boa", imagens: "/imagens/camisa_preta.png"},
+  {id: 9, nome: "Camisa Nike Vinho", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_vinho.png"},
+  {id: 10, nome: "Camisa Nike Azul", preco: "R$ 220", descricao: "Camisa bem boa", imagens: "/imagens/camisa_azul.png"},
 ]
 
 function buscarProdutosPorID (id){
@@ -28,8 +28,9 @@ app.get('/', (req, res) => {
   res.render('home', { produtos });
 });
 
-app.get('/produto', (req, res) => {
-  res.render('produto', { message: 'Olá, kjyuoyiu!' });
+app.get('/produto/:id', (req, res) => {
+  const produto = buscarProdutosPorID(req.params.id)
+  res.render('produto', { produto });
 });
 
 app.listen(port, () => {
